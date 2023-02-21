739aae23c788508e0b758a83cde995081e0a9ef9
László Séra <laszlo.sera@gmail.com>
2023-02-21 15:11:09 +0100

    Creating a message publisher

---
+++ 1.Sqs/SqsPublisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "john@doe.com",
+    FullName = "John Doe",
+    GitHubUsername = "johndoe",
+    DateOfBirth = new DateOnly(1991, 12, 22)
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+  QueueUrl  = queueUrlResponse.QueueUrl,
+  MessageBody = JsonSerializer.Serialize(customer),
+  MessageAttributes = new Dictionary<string, MessageAttributeValue>
+  {
+      {
+          "MessageType", new MessageAttributeValue
+          {
+              DataType = "String",
+              StringValue = nameof(CustomerCreated)
+          }
+      }
+  }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine();
