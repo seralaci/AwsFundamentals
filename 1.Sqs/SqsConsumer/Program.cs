3751a67b90097c1a94751cc8facaa3c8bf6c6194
László Séra <laszlo.sera@gmail.com>
2023-02-21 15:31:06 +0100

    Creating a message consumer

---
+++ 1.Sqs/SqsConsumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var cancellationTokenSource = new CancellationTokenSource();
+var sqsClient = new AmazonSQSClient();
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    AttributeNames = new List<string>{ "All" },
+    MessageAttributeNames = new List<string>{ "MessageType" } // All can also be used here
+};
+
+while (!cancellationTokenSource.IsCancellationRequested)
+{
+    var response = await sqsClient.ReceiveMessageAsync(receiveMessageRequest, cancellationTokenSource.Token);
+
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine($"Message Id: {message.MessageId}");
+        Console.WriteLine($"Message Body: {message.Body}");
+
+        await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle);
+    }
+    
+    await Task.Delay(3000);
+}
