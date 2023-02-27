d097a3db03c51c66fb09c5d1e9629dcd3101f461
László Séra <laszlo.sera@gmail.com>
2023-02-27 16:35:10 +0100

    AWS Lambda examples

--- 5.SecretsManager/Weather.Api/Program.cs
+++ 5.SecretsManager/Weather.Api/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 var env = builder.Environment.EnvironmentName;
 var appName = builder.Environment.ApplicationName;
 
+// The name of key in AWS is: Development_Weather.Api_OpenWeatherMapApi__ApiKey
 builder.Configuration.AddSecretsManager(configurator: options =>
 {
     options.SecretFilter = entry => entry.Name.StartsWith($"{env}_{appName}");
